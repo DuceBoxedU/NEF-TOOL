8a7dcd8d9855431215f2c7dd297845ec5f8b45e2
Duce Boxed U <106915154+DuceBoxedU@users.noreply.github.com>
2022-07-18 23:01:34 +0200

    Add files via upload

---
+++ NefTool/Token Generator/Nef Tool.bat
@@ -0,0 +1,64 @@
+@echo off
+title Nef Tool
+echo                                                          ;::::;                              
+echo                                                        ;::::; :;                              
+echo                                                      ;:::::'   :;                             
+echo                                                     ;:::::;     ;.                           
+echo                                                    ,:::::'  .  . ;           OOO\           
+echo                                                    ::::::;       ;          OOOOO\            
+echo                                                    ;:::::;       ;         OOOOOOOO           
+echo                                                   ,;::::::;     ;'         / OOOOOOO          
+echo                                                 ;:::::::::`. ,,,;.        /  / DOOOOOO        
+echo                                               .';:::::::::::::::::;,     /  /     DOOOO     
+echo                                              ,::::::;::::::;;;;::::;,   /  /        DOOO     
+echo                                             ;`::::::`'::::::;;;::::: ,#/  /          DOOO   
+echo                                             :`:::::::`;::::::;;::: ;::#  /            DOOO   
+echo                                             ::`:::::::`;:::::::: ;::::# /              DOO   
+echo                                             `:`:::::::`;:::::: ;::::::#/               DOO
+echo                                              :::`:::::::`;; ;:::::::::##                OO
+echo                                              ::::`:::::::`;::::::::;:::#                OO
+echo                                              `:::::`::::::::::::;'`:;::#                O
+echo                                               `:::::`::::::::;' /  / `:#
+echo                                                ::::::`:::::;'  /  /   `#     
+echo                         ______   ______     __  __     ______ /  /__   __        ______     ______     __   __    
+echo                        /\__  _\ /\  __ \   /\ \/ /    /\  ___\  //\ "-.\ \      /\  ___\   /\  ___\   /\ "-.\ \   
+echo                        \/_/\ \/ \ \ \/\ \  \ \  _"-.  \ \  __\ / \ \ \-.  \     \ \ \__ \  \ \  __\   \ \ \-.  \  
+echo                           \ \_\  \ \_____\  \ \_\ \_\  \ \_____\  \ \_\\"\_\     \ \_____\  \ \_____\  \ \_\\"\_\ 
+echo                            \/_/   \/_____/   \/_/\/_/   \/_____/   \/_/ \/_/      \/_____/   \/_____/   \/_/ \/_/                                                                                                                                                                                                                                                                            
+echo                                                By .exe#1734
+echo                                                Join: https://discord.gg/RtecqyJNyZ                                
+
+echo NEF TOOL
+
+echo Starting Tool
+ping -n 5 127.0.0.1 > nul
+echo.&echo.&echo.
+echo                                   Starting Token Generator...
+echo                                   (only email verified)
+echo                                   -wait a few seconds
+ping -n 5 127.0.0.1 > nul
+echo ricerca di aggiornamenti...
+ping -n 5 127.0.0.1 > nul
+echo nessun aggiornamento! versione 0.1
+ping -n 5 127.0.0.1 > nul
+explorer https://tempmailo.com/
+echo Browser aperto con successo!
+ping -n 6 127.0.0.1 > nul
+ping -n 20 127.0.0.1 > nul
+echo Creazione Email...
+ping -n 5 127.0.0.1 > nul
+echo Attendi 10 secondi,
+ping -n 5 127.0.0.1 > nul
+echo Email creata!
+echo impossibile copiare l'email (error)
+echo per favore copia l'email
+ping -n 5 127.0.0.1 > nul
+explorer https://discord.com/login
+ping -n 5 127.0.0.1 > nul
+echo apertura discord...
+echo Login creato con successo! Personalizza il tuo token!
+ping -n 20 127.0.0.1 > nul
+echo bypass della verifica fallito...
+ping -n 10 127.0.0.1 > nul
+echo Clicca "enter" per chiudere
+pause >nul
